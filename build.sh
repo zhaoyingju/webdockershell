cefa3c583674ce19896164428f4ea916b02eaa2d
micro <micro@163.com>
2019-06-20 17:34:54 +0800

    d

---
+++ build.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+echo ">>>cp build.sh"
+cp /var/code/webdockershell/build.sh /var/code/build.sh
+
+echo ">>>chmod build.sh"
+chmod +x /var/code/build.sh
+
+echo ">>>start build"
+/bin/bash /var/code/build.sh
