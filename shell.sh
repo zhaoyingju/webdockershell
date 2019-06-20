25d175807ce18e584303240827b48377dfcc2e4a
micro <micro@163.com>
2019-06-20 16:35:42 +0800

    d

--- shell.sh
+++ shell.sh
@@ -1,13 +1,6 @@
 #/bin/sh
 # git yum安装 检查git 是否存在 存在不安装
 git --version
-if [ $? -ne 0 ];then
- yum install git
- if [ $? -ne 0 ];then
-  echo "yum安装失败！"
-  exit 0
- fi
-fi
 
 #设置user and email
 git_url="https://github.com/zhaoyingju/webdockershell.git"
