196bfa78e37f46c322fdb8af65951f3ce04a1649
micro <micro@163.com>
2019-06-20 16:54:26 +0800

    d

--- Program.cs
+++ Program.cs
@@ -20,6 +20,6 @@ namespace webshell
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)        
                 .UseUrls("http://*:5000")    
-                .UseStartup<Startup>().;
+                .UseStartup<Startup>();
     }
 }
