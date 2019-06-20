e9760cb71186cdbf99d27340e9c117bba66582c0
micro <micro@163.com>
2019-06-20 17:37:29 +0800

    d

--- build.sh
+++ build.sh
@@ -1,10 +1,28 @@
 #/bin/sh
+# git yum安装 检查git 是否存在 存在不安装
 
-echo ">>>cp build.sh"
-cp /var/code/webdockershell/build.sh /var/code/build.sh
+git --version
+#设置user and email
+git_url="https://github.com/zhaoyingju/webdockershell.git"
+username="zhaoyingju"
+email="zhaoyingju@163.com"
+path="/var/code/webdockershell"
+git config -- user.name $username
+git config  user.email $email
 
-echo ">>>chmod build.sh"
-chmod +x /var/code/build.sh
+#验证录入的地址是否存在 不存在创建
+if [ ! -d "$path" ];then
+ mkdir -p $path
+ cd $path
+ git init
+fi
+ cd $path
 
-echo ">>>start build"
-/bin/bash /var/code/build.sh
+# 拉取代码
+echo '>>>拉取代码'
+git pull $git_url
+
+echo '执行dotnet编译发布'
+dotnet restore
+dotnet build
+dotnet publish
